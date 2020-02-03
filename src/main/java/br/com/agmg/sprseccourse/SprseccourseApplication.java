6c78acc87afdf65724b284ea45873cd98db6035c
AlexandreGuerra <agmguerra@gmail.com>
2020-02-03 08:24:47 -0300

    initial

---
+++ src/main/java/br/com/agmg/sprseccourse/SprseccourseApplication.java
@@ -0,0 +1,13 @@
+package br.com.agmg.sprseccourse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SprseccourseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SprseccourseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/agmg/sprseccourse/SprseccourseApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.agmg.sprseccourse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SprseccourseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
